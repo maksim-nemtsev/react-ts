bb625a0999abe4712612bfc625d257835008a1c7
Maksim <vezunchigg@gmail.com>
2021-11-07 01:10:40 +0000

    feat:-add react hooks;

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import "./App.css";
+
+/* Rules:
+1.Allways use the setter for useState
+2.Don't depend on data you set in [] useEffect()
+ */
+function MyHooksComponents() {
+  console.log("MyHooksComponents started");
+  const [numbers, setNumbers] = useState<number[]>([]);
+  console.log(`MyHooksComponents numbers= ${JSON.stringify(numbers)}`);
+
+  useEffect(() => {
+    fetch("./numbers.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(`data = ${JSON.stringify(data)}`);
+        setNumbers(data);
+        console.log("request finished")
+      });
+  }, []);
+
+  //[1,2,3] => [1] with no dep([])
+  const addOne = useCallback( () => {
+    // setNumbers([...numbers, numbers.length + 1])
+    setNumbers(currentNumbers => [
+      ...currentNumbers, currentNumbers.length + 1
+    ])
+  }, [])
+
+  // const sum = numbers.reduce((a, v) => a + v , 0)
+  const sum = useMemo(() => numbers.reduce((a, v) => a + v , 0), [numbers]);
+
+  console.log("MyHooksComponents render");
+  const out = (
+    <div>
+      <div>Numbers: {JSON.stringify(numbers)}</div>
+      <button onClick={() => addOne()}>
+        add one element in Array
+      </button>
+      <div>
+      <span>
+        {`The sum is ${sum}`}
+      </span>
+      </div>
+    </div>
+  );
+  console.log("MyHooksComponents finished");
+  return out;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyHooksComponents />
     </div>
   );
 }
